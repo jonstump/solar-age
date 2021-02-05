c2ae255d334df4e3b4992ea189f5c768da6af859
Jon Stump <jmstump@gmail.com>
2021-02-05 13:33:32 -0800

    good pass for updated Human constructor.

--- src/js/age.js
+++ src/js/age.js
@@ -1,7 +1,5 @@
 //Note to instructor: I started my project in a different order at the beginning of the day and realized I needed to pivot back to some simpler code. This might look a little wonky in my git commit history as it made me realize I needed to update my constructor in order to significantly dry up my code. Just wanted to leave this note here in case there was some confusing looking through my git history.
 
-import { thisExpression } from "@babel/types";
-
 export class Human {
   constructor (age, birthGender) {
     this.age = age;
@@ -11,10 +9,16 @@ export class Human {
     this.venusAge = 0;
     this.marsAge = 0;
     this.jupiterAge = 0;
+    this.mercuryExpectancy = 0;
+    this.venusExpectancy = 0;
+    this.marsExpectancy = 0;
+    this.jupiterExpectancy = 0;
   }
   insterstellarAge() {
     this.mercuryAge = Math.round(this.age/.24);
     this.venusAge = Math.round(this.age/.62);
+    this.marsAge = Math.round(this.age/1.88);
+    this.jupiterAge = Math.round(this.age/11.86);
   }
   earthExpectancy() {
     if (this.birthGender === "female" && this.age < 65) {
@@ -31,30 +35,33 @@ export class Human {
       return this.earthAvgLife;
     }
   }
-  mercuryExpectancy() {
-    if (this.birthGender === "female" && this.age < 65) {
-      const mercAvgLife = this.earthAvgLife * 4.32;
-      //these need to be fixed to 4.16 (it's 2021 not 2020)
-      return parseInt(mercAvgLife);
-    } else if (this.birthGender === "female" && this.age > 65) {
-      const mercAvgLife = this.earthAvgLife * 4.32;
-      return parseInt(mercAvgLife);
-    } else if (this.birthGender === "male" && this.age > 65) {
-      const mercAvgLife = this.earthAvgLife * 4.32;
-      return parseInt(mercAvgLife);
-    } else {
-      const mercAvgLife = this.earthAvgLife * 4.32;
-      return parseInt(mercAvgLife);
-    }
+  insterstellarExpectancy() {
+
   }
-  venusExpectancy() {
-    const venusAvgLife = this.earthAvgLife * 1.628;
-    return parseInt(venusAvgLife);
-  }
-  marsExpectancy() {
-    const marsAvgLife = this.earthAvgLife * .532;
-    return parseInt(marsAvgLife);
-  }
+  // mercuryExpectancy() {
+  //   if (this.birthGender === "female" && this.age < 65) {
+  //     const mercAvgLife = this.earthAvgLife * 4.32;
+  //     //these need to be fixed to 4.16 (it's 2021 not 2020)
+  //     return parseInt(mercAvgLife);
+  //   } else if (this.birthGender === "female" && this.age > 65) {
+  //     const mercAvgLife = this.earthAvgLife * 4.32;
+  //     return parseInt(mercAvgLife);
+  //   } else if (this.birthGender === "male" && this.age > 65) {
+  //     const mercAvgLife = this.earthAvgLife * 4.32;
+  //     return parseInt(mercAvgLife);
+  //   } else {
+  //     const mercAvgLife = this.earthAvgLife * 4.32;
+  //     return parseInt(mercAvgLife);
+  //   }
+  // }
+  // venusExpectancy() {
+  //   const venusAvgLife = this.earthAvgLife * 1.628;
+  //   return parseInt(venusAvgLife);
+  // }
+  // marsExpectancy() {
+  //   const marsAvgLife = this.earthAvgLife * .532;
+  //   return parseInt(marsAvgLife);
+  // }
 }
 
 //a quick note on gender. Since I am unable to find life expectancy calculators that do not include genders past male and female I decided to make my gender "birth gender" for the sake of purposes to attempt to be more inclusive. If I could do a calculation that allowed for more options I would do so.
