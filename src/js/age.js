3a52aeb7bc541dc1aa84226c93fc7800447e257a
Jon Stump <jmstump@gmail.com>
2021-02-05 08:48:26 -0800

    pass test for Human constructor creation.

--- src/js/age.js
+++ src/js/age.js
@@ -1,3 +1,8 @@
 export class Human {
-  
+  constructor (age, birthGender) {
+    this.age = age;
+    this.birthGender = birthGender;
+  }
 }
+
+//a quick note on gender. Since I am unable to find life expectancy calculators that do not include genders past male and female I decided to make my gender "birth gender" for the sake of purposes to attempt to be more inclusive. If I could do a calculation that allowed for more options I would do so.
