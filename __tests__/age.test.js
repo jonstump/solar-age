bd70edad147944d4879eb1200095dc634c031dba
Jon Stump <jmstump@gmail.com>
2021-02-05 09:18:10 -0800

    good fail for test on earthExpectancy

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -1,10 +1,18 @@
 import { Human } from '../src/js/age.js';
 
-describe('Age', () => {
+describe('Human', () => {
 
   test('should correctly create an object with user age and birth gender', () => {
     const human = new Human (25, "female");
     expect(human.age).toEqual(25);
     expect(human.birthGender).toEqual("female");
+  })
+})
+
+describe('Life Expectancy per Planet', () => {
+
+  test('should correctly calculate life expectancy on Earth', () => {
+    const human = new Human (25, "female");
+    expect(human.earthExpectancy()).toEqual(82);
   })
 })
