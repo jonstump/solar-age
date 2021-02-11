ab69db8ed5416fd7187b4dd296aae72ac39e07ef
Jon Stump <jmstump@gmail.com>
2021-02-10 17:58:54 -0800

    good pass on Age Alert for Mars

--- src/age.js
+++ src/age.js
@@ -73,5 +73,6 @@ export class UserStrings {
     this.earth = `You are ${human.age} years old and have a life expectancy of ${human.earthAvgLife} years`;
     this.mercury = `You are ${human.mercuryAge} years old and have a life expectancy of ${human.mercuryExpectancy} years`;
     this.venus = `You are ${human.venusAge} years old and have a life expectancy of ${human.venusExpectancy} years`;
+    this.mars = `You are ${human.marsAge} years old and have a life expectancy of ${human.marsExpectancy} years`;
   }
 }
